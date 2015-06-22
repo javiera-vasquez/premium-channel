cb7b7a21e1c2f9fbf1a7cc7acea35a8de1c6e066
Javier Enrique <s0wkete@gmail.com>
2015-06-22 17:43:21 -0300

    added loop of channel

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,13 +3,14 @@
 // Document ready
 $(function() {
 	// Calc the height of the viewport and pass to the #vtr landing
-  	var windowsH = $(window).height() - ($('.toolbar').height() + $('.divisor').height() + 30);
-	$('#vtr').css('height', windowsH + 'px');
+  	var windowsHeight = $(window).height() - ($('.toolbar').height() + $('.divisor').height() + 30),
+  		animationNav = 'animated fadeIn active';
+	$('#vtr').css('height', windowsHeight + 'px');
 	// Check the scroll top, and remove negative margin to .header
 	$(window).on('scroll', function() {
 		// if($(window).scrollTop() < 0) $('.header').css('margin-top',$(window).scrollTop() + 'px');
-		if($(window).scrollTop() >= 44) $('.header').addClass('animated fadeIn active');
-		else if($(window).scrollTop() <= 44) $('.header').removeClass('animated fadeIn active');
+		if($(window).scrollTop() >= 44) $('.header').addClass(animationNav);
+		else if($(window).scrollTop() <= 44) $('.header').removeClass(animationNav);
 	})
 });
 
