6e3365625153286c8ba8837c8ae037befe3a32e4
Javier Enrique <s0wkete@gmail.com>
2015-06-22 16:15:12 -0300

    added base template and sass
    Details of changes:
    -Fixed nav
    -Windows height for carrousel
    -Sass for carrousel and divisor

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('\'Allo \'Allo!');
+// Watch scroll for active element
+//$('#header').scrollspy({ target: '#navbar-example' });
+// Document ready
+$(function() {
+	// Calc the height of the viewport and pass to the #vtr landing
+  	var windowsH = $(window).height() - ($('.toolbar').height() + $('.divisor').height() + 30);
+	$('#vtr').css('height', windowsH + 'px');
+	// Check the scroll top, and remove negative margin to .header
+	$(window).on('scroll', function() {
+		// if($(window).scrollTop() < 0) $('.header').css('margin-top',$(window).scrollTop() + 'px');
+		if($(window).scrollTop() >= 44) $('.header').addClass('animated fadeIn active');
+		else if($(window).scrollTop() <= 44) $('.header').removeClass('animated fadeIn active');
+	})
+});
+
+
+
+
